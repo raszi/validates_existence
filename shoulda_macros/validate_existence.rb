3c4154b4ae99fdcce5da70175a5f63ceb993a703
Sean Huber <shuber@huberry.com>
2009-01-17 22:46:30 -0800

    Update shoulda_macros

--- shoulda_macros/validate_existence.rb
+++ shoulda_macros/validate_existence.rb
@@ -1,38 +1,28 @@
 class Test::Unit::TestCase
 
-  def self.should_require_existence_of(*attrs)
+  def self.should_require_existence_of(*attributes)
+    options = attributes.extract_options!
     klass = model_class
-    options = attrs.last.is_a?(Hash) ? attrs.pop : {}
-    attrs.each do |attribute|
-      attribute_instance = Factory(attribute)
-      
-      should "be valid when #{attribute} exists" do
-        @klass = klass.new "#{attribute}_id".to_sym => attribute_instance.id
-        @klass.valid?
-        assert !@klass.errors.on(attribute)
+    
+    attributes.each do |attribute|
+      should "require #{attribute} exists" do
+        object = get_instance_of(klass)
+        object.send("#{attribute}_id=", 0)
+        assert !object.valid?, "#{object.class} was saved with a non-existent #{attribute}"
+        assert object.errors.on(attribute), "There are no errors on #{attribute} after being set to a non-existent record"
+        assert_contains(object.errors.on(attribute), 'does not exist', "when set to 0")
       end
-      
-      should "require existence of #{attribute}" do
-        @klass = klass.new "#{attribute}_id".to_sym => 0
-        assert !@klass.valid?
-        assert @klass.errors.on(attribute)
-      end
-      
-      if options[:allow_nil] || options[:allow_blank]
-        should "not require existence of #{attribute} if #{attribute}_id is blank" do
-          @klass = klass.new "#{attribute}_id".to_sym => nil
-          @klass.valid?
-          assert !@klass.errors.on(attribute)
-        end
-      else
-        should "require existence of #{attribute} if #{attribute}_id is blank" do
-          @klass = klass.new "#{attribute}_id".to_sym => nil
-          assert !@klass.valid?
-          assert @klass.errors.on(attribute)
+    end
+    
+    if options[:allow_nil]
+      attributes.each do |attribute|
+        should "allow #{attribute} to be nil" do
+          object = get_instance_of(klass)
+          object.send("#{attribute}_id=", nil)
+          object.valid?
+          assert !object.errors.on(attribute), "There were errors on #{attribute} after being set to nil"
         end
       end
-      
-      attribute_instance.destroy
     end
   end
   
