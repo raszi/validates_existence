4b9e019d1d35b9c6a651a83406ce7b85c76920a1
Rob Anderton <rob.anderton@thewebfellas.com>
2010-04-23 14:31:47 +0100

    update shoulda macro to use subject

--- shoulda_macros/validates_existence.rb
+++ shoulda_macros/validates_existence.rb
@@ -2,28 +2,33 @@ class Test::Unit::TestCase
 
   def self.should_require_existence_of(*attributes)
     options = attributes.extract_options!
-    klass = model_class
-    
+    options[:message] ||= "does not exist"
+
     attributes.each do |attribute|
       should "require #{attribute} exists" do
-        object = get_instance_of(klass)
-        object.send("#{attribute}_id=", 0)
-        assert !object.valid?, "#{object.class} was saved with a non-existent #{attribute}"
-        assert object.errors.on(attribute), "There are no errors on #{attribute} after being set to a non-existent record"
-        assert_contains(object.errors.on(attribute), 'does not exist', "when set to 0")
+        subject.send("#{attribute}_id=", 0)
+        assert !subject.valid?, "#{subject.class} was saved with a non-existent #{attribute}"
+        assert subject.errors.on(attribute), "There are no errors on #{attribute} after being set to a non-existent record"
+        assert_contains(subject.errors.on(attribute), options[:message], "when set to 0")
       end
     end
-    
-    if options[:allow_nil]
-      attributes.each do |attribute|
+
+    attributes.each do |attribute|
+      if options[:allow_nil]
         should "allow #{attribute} to be nil" do
-          object = get_instance_of(klass)
-          object.send("#{attribute}_id=", nil)
-          object.valid?
-          assert !object.errors.on(attribute), "There were errors on #{attribute} after being set to nil"
+          subject.send("#{attribute}_id=", nil)
+          subject.valid?
+          assert !subject.errors.on(attribute), "There were errors on #{attribute} after being set to nil"
+        end
+      else
+        should "not allow #{attribute} to be nil" do
+          subject.send("#{attribute}_id=", nil)
+          assert !subject.valid?, "#{subject.class} was saved with a nil #{attribute}"
+          assert subject.errors.on(attribute), "There were errors on #{attribute} after being set to nil"
+          assert_contains(subject.errors.on(attribute), options[:message], "when set to nil")
         end
       end
     end
   end
-  
+
 end
